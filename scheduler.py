66caab54cace9b5a1ddd2b2687dc1155556ca044
mateosierens <mateo.sierens@student.uantwerpen.be>
2021-02-25 19:32:08 +0100

    removed TODO

--- scheduler.py
+++ scheduler.py
@@ -3,6 +3,6 @@ from models import System, Intersection
 def scheduler(system):
     for intersection in system.intersections:
         for incoming_street in intersection.incoming:
-            # TODO: schedule incoming street in round robin
+            # schedule incoming street in round robin
             intersection.schedule[incoming_street] = 1
     return system
