ca6941924117fa46cd3214c500c95ed3c067393d
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2021-02-25 20:28:33 +0100

    Main runs all files

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from scheduler import scheduler
 from output import generate_output
 
 if __name__ == '__main__':
-    filename = "a.txt"
-    system = read_file(f"input/{filename}")
-    system = scheduler(system)
-    generate_output(system, f"output/generated_{filename}")
-    print(system)
+    for letter in "abcdef":
+        filename = f"{letter}.txt"
+        system = read_file(f"input/{filename}")
+        system = scheduler(system)
+        generate_output(system, f"output/generated_{filename}")
+        print(system)
