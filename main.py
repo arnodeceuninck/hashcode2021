3231c6d5a128ffbd0954357fa45830527fef22e2
basil <basil.rommens@student.uantwerpen.be>
2021-02-25 20:30:04 +0100

    fixed better output

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from output import generate_output
 
 if __name__ == '__main__':
     for letter in "abcdef":
+        print('Starting: ', letter)
         filename = f"{letter}.txt"
         system = read_file(f"input/{filename}")
         system = scheduler(system)
         generate_output(system, f"output/generated_{filename}")
-        print(system)
+        print('Finishing: ', letter)
